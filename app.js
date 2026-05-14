a5a043b2168751ca08ce017188e60b10ea5dca14
naodiw <nao.dss@gmail.com>
2026-05-14 18:01:46 +0700

    Restore initial setView so river layers can render

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ function addRivers(geojson) {
 }
 
 function setupMap() {
-  state.map = L.map("map", { zoomControl: false, fadeAnimation: false, zoomAnimation: false });
+  state.map = L.map("map", { zoomControl: false, fadeAnimation: false, zoomAnimation: false }).setView([20.05, 99.85], 9);
   L.control.zoom({ position: "bottomright" }).addTo(state.map);
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
